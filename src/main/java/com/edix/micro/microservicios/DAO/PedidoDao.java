b51d46daf63dccbe3ec77394b8fb9b6ccf39c94e
David González Ramiro <davidgramiro@gmail.com>
2023-01-31 18:46:41 +0100

    Update readme and add img files

--- src/main/java/com/edix/micro/microservicios/DAO/PedidoDao.java
+++ src/main/java/com/edix/micro/microservicios/DAO/PedidoDao.java
@@ -2,15 +2,14 @@ package com.edix.micro.microservicios.DAO;
 
 import com.edix.micro.microservicios.EntityBeans.Pedido;
 import com.edix.micro.microservicios.Repository.PedidoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-@Service
-public class PedidoDao implements IntPedidoDao{
 
-    final
-    PedidoRepository p;
+@Service
+public class PedidoDao implements IntPedidoDao {
+
+    final PedidoRepository p;
 
     public PedidoDao(PedidoRepository p) {
         this.p = p;
